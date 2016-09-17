e5058043bdd9bed0391486483c864dac1b6ee924
zhaixiaotong <zhxt198811@126.com>
2016-09-17 16:23:38 +0800

    1.2  9.17

--- src/main/java/com/model/Constants.java
+++ src/main/java/com/model/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public final static String ROLE_USER = "02";// 注册用户权限，暂时用不上
     public final static String ROLE_TRUCK = "03";// 此角色提供给卡车司机，可以发布车辆信息，找货，找集装箱
     public final static String ROLE_CARGO = "04";// 此角色提供给货物商家，可以发布货源，找车
-    public final static String ROLE_CONTAINER = "05";// 此角色提供给货物商家，可以发布集装箱，找车
+    public final static String ROLE_CONTAINER = "05";// 目前只用04，这个暂时不用，此角色提供给货物商家，可以发布集装箱，找车
     public final static String ROLE_VISITOR= "06";// 游客权限
 
 }
